419534ab8f7a88b742e13fca2842e1f34cba1d5b
Martin Di Paola <martinp.dipaola@gmail.com>
2016-08-11 00:27:27 -0300

    Added an example about the lib: this time about how to pack/unpack an ARP packet.

--- examples/arp.py
+++ examples/arp.py
@@ -19,11 +19,27 @@ class ARP(Packet):
 
 if __name__ == '__main__':
    from base64 import b16decode
-   from utils import inspect
+   from utils import inspect, hd
 
-   raw_query = b16decode('0001000206040000efefefefefef000000001f1f1f1f1f1f00000000', True)
-   
+   raw_query = b16decode('00010800060400010018f7f6f7fdc0a80103000000000000c0a8010c', True)
+   raw_response = b16decode('0001080006040002002f6f5fdfdfc0a8010c0018f7f6f7fdc0a80103', True)
+
    arp_query = ARP()
-   arp_query.unpack(raw_query)
+   query_last = arp_query.unpack(raw_query)
 
-   inspect(arp_query)
+   arp_response = ARP()
+   response_last = arp_response.unpack(raw_response)
+
+
+   assert arp_query.hw_type == arp_response.hw_type == 0x0001        # ethernet
+   assert arp_query.prot_type == arp_response.prot_type == 0x0800    # ip
+
+   assert arp_query.hw_len == arp_response.hw_len == 6
+   assert arp_query.prot_len == arp_response.prot_len == 4
+
+   assert arp_query.sender_hw_addr == arp_response.target_hw_addr == b16decode('0018f7f6f7fd', True)
+   assert arp_query.sender_prot_addr == arp_response.target_prot_addr == b16decode('c0a80103', True)
+
+   assert arp_query.target_hw_addr == b16decode('000000000000', True)
+   assert arp_response.sender_hw_addr == b16decode('002f6f5fdfdf', True)
+   assert arp_query.target_prot_addr == arp_response.sender_prot_addr == b16decode('c0a8010c', True)
