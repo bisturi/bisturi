12c265ad321b0420f22256e9dd485952f62c8c1b
Martin Di Paola <martinp.dipaola@gmail.com>
2016-08-11 00:27:25 -0300

    Small fix in the pcap example.

--- stuff/pcap.py
+++ stuff/pcap.py
@@ -10,23 +10,23 @@ class MagicNumber(Packet):
       return self.magic_number in ('\xa1\xb2\xc3\xd4', '\xa1\xb2\x3c\x4d')
 
 
-def get_header_and_packet_class(magic_number):
-   endianess = 'big' if magic_number.are_bytes_in_network_order() else 'little'
+   def get_header_and_packet_classes(self):
+      endianess = 'big' if self.are_bytes_in_network_order() else 'little'
 
-   class Header(Packet):
-      major_version = Int(2, endianess=endianess, default=2)
-      minor_version = Int(2, endianess=endianess, default=4)
-      time_zone_offset = Int(4, endianess=endianess, default=0)
-      timestamp_accuracy = Int(4, endianess=endianess, default=0)
-      snapshot_length = Int(4, endianess=endianess)
-      link_layer_header_type = Int(4, endianess=endianess)
+      class Header(Packet):
+         major_version = Int(2, endianess=endianess, default=2)
+         minor_version = Int(2, endianess=endianess, default=4)
+         time_zone_offset = Int(4, endianess=endianess, default=0)
+         timestamp_accuracy = Int(4, endianess=endianess, default=0)
+         snapshot_length = Int(4, endianess=endianess)
+         link_layer_header_type = Int(4, endianess=endianess)
 
-   
-   class Packet_(Packet):
-      timestamp_approx = Int(4, endianess=endianess)
-      timestamp = Int(4, endianess=endianess)
-      length = Int(4, endianess=endianess)
-      untruncated_length = Int(4, endianess=endianess)
-      raw = Data(length)
+      
+      class Packet_(Packet):
+         timestamp_approx = Int(4, endianess=endianess)
+         timestamp = Int(4, endianess=endianess)
+         length = Int(4, endianess=endianess)
+         untruncated_length = Int(4, endianess=endianess)
+         raw = Data(length)
 
-   return Header, Packet_
+      return Header, Packet_
