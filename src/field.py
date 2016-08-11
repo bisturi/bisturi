dc4361668362bbb75ef18083cc9a5572bff6f5f5
Martin Di Paola <martinp.dipaola@gmail.com>
2016-08-11 00:27:24 -0300

    Implemented the parsing of a string creating a packet instance.

--- src/field.py
+++ src/field.py
@@ -1,36 +1,80 @@
-import time
+import time, struct, sys
 
 class Field(object):
    def __init__(self):
       self.ctime = time.time()
 
-   def compile(self):
+   def compile(self, field_name):
       del self.ctime
+      self.field_name = field_name
+      self.getval = lambda packet: getattr(packet, field_name)
+      self.setval = lambda packet, val: setattr(packet, field_name, val)
+
 
    def init(self, packet, field_name, defaults):
       raise NotImplementedError()
 
-   '''def __get__(self, instance, owner):
+   def from_raw(self, packet, raw, offset=0):
       raise NotImplementedError()
 
-   def __set__(self, instance, obj):
-      raise NotImplementedError()
-      '''
 
 
 class Int(Field):
    def __init__(self, byte_count=4, signed=False, endianess='big', default=0):
       Field.__init__(self)
       self.default = default
+      self.byte_count = byte_count
+      self.is_signed = signed
+      self.is_bigendian = (endianess in ('big', 'network')) or (endianess == 'local' and sys.byteorder == 'big')
+      self.base = 2**(byte_count*8) - 1 #base minus 1
 
-   def init(self, packet, field_name, defaults):
-      setattr(packet, field_name, defaults.get(field_name, self.default))
+   def init(self, packet, defaults):
+      self.setval(packet, defaults.get(self.field_name, self.default))
+
+   def from_raw(self, packet, raw, offset=0):
+      raw_data = raw[offset:offset+self.byte_count]
+
+      if self.byte_count in (1, 2, 4, 8): 
+         code = {1:'B', 2:'H', 4:'I', 8:'Q'}[self.byte_count]
+         if self.is_signed:
+            code.lowercase()
+
+         fmt = (">" if self.is_bigendian else "<") + code
+         s = struct.Struct(fmt)
+
+         integer = s.unpack(raw_data)[0]
+
+      else:
+         if not self.is_bigendian:
+            raw_data = raw_data[::-1]
+         
+         num = int(raw_data.encode('hex'), 16)
+         if self.is_signed and ord(raw_data[0]) > 127:
+            num = -(self.base - num)
+
+         integer = num
+
+      self.setval(packet, integer)
+
+      return self.byte_count
 
 
 class Data(Field):
    def __init__(self, byte_count, default=''):
       Field.__init__(self)
       self.default = default
+      self.byte_count = byte_count
 
-   def init(self, packet, field_name, defaults):
-      setattr(packet, field_name, defaults.get(field_name, self.default))
+   def init(self, packet, defaults):
+      self.setval(packet, defaults.get(self.field_name, self.default))
+
+   def from_raw(self, packet, raw, offset=0):
+      if isinstance(self.byte_count, int):
+         count = self.byte_count
+      else:
+         count = self.byte_count.getval(packet)
+
+      raw_data = raw[offset:offset+count]
+      self.setval(packet, raw_data)
+
+      return count
--- src/packet.py
+++ src/packet.py
@@ -11,7 +11,7 @@ class MetaPacket(type):
       fields = filter(lambda name_val: isinstance(name_val[1], Field), attrs.iteritems())
       fields.sort(key=lambda name_val: name_val[1].ctime)
       
-      map(lambda name_val: name_val[1].compile(), fields)
+      map(lambda name_val: name_val[1].compile(name_val[0]), fields)
 
       @classmethod
       def get_fields(cls):
@@ -22,14 +22,16 @@ class MetaPacket(type):
 class Packet(object):
    __metaclass__ = MetaPacket
 
-   def __init__(self, **defaults):
-      map(lambda name_val: name_val[1].init(self, name_val[0], defaults), self.get_fields())
+   def __init__(self, bytestring=None, **defaults):
+      map(lambda name_val: name_val[1].init(self, defaults), self.get_fields())
+      
+      if bytestring is not None:
+         self.from_raw(bytestring)
 
 
    def from_raw(self, raw, offset=0):
-      cls = self.__class__
-      for name, f in cls.get_fields():
-         offset += f.from_raw(raw[offset:])
+      for name, f in self.get_fields():
+         offset += f.from_raw(self, raw[offset:])
 
       return offset
          
