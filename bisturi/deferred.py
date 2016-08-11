fd7153e2310b8f9b81d98d9608fef5ed0beddb96
Martin Di Paola <martinp.dipaola@gmail.com>
2016-08-11 00:27:32 -0300

    Added support for logic operation and other unary operations (like negations). Fixed a bug in the names of some methods of the operations that don't match with the function names like add, or, .... Made the expression a fully proxy too.

--- bisturi/deferred.py
+++ bisturi/deferred.py
@@ -53,10 +53,11 @@ def _defer_method(target, methodname, op, is_binary):
    if is_binary:
       setattr(target, methodname, lambda A, B: BinaryExpr(A, B, op))
    else:
-      setattr(target, methodname, lambda A: UnaryExpr(A, B, op))
+      setattr(target, methodname, lambda A: UnaryExpr(A, op))
    
 
 def defer_operations_of(cls):
+   # TODO - revisar todos los nombres
    for binary_op in [
             # arith ------------------------------------
             operator.add,         operator.sub,
@@ -68,9 +69,39 @@ def defer_operations_of(cls):
             operator.le,          operator.lt,
             operator.ge,          operator.gt,
             operator.eq,          operator.ne,
+
+            # logical ----------------------------------
+            operator.and_,        operator.or_,
+            operator.xor,              
+            operator.rshift,      operator.lshift,
             ]:
       
-      methodname = "__%s__" % binary_op.__name__
+      op_name = binary_op.__name__
+      if op_name.endswith("_"):
+         op_name = op_name[:-1]
+
+      methodname = "__%s__" % op_name
       _defer_method(cls, methodname, binary_op, is_binary=True)
    
+   for unary_op in [
+            # arith ------------------------------------
+            operator.neg,
+
+            # logical ----------------------------------
+            operator.not_,        operator.inv,
+            ]:
+      
+      op_name = unary_op.__name__
+      if op_name.endswith("_"):
+         op_name = op_name[:-1]
+
+      if op_name == "inv":
+         op_name = "invert"
+
+      methodname = "__%s__" % op_name
+      _defer_method(cls, methodname, unary_op, is_binary=False)
+   
    return cls
+
+UnaryExpr = defer_operations_of(UnaryExpr)
+BinaryExpr = defer_operations_of(BinaryExpr)
