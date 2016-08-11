5524400fca6aaa9c36a2714e7464da8f434efd48
Martin Di Paola <martinp.dipaola@gmail.com>
2016-08-11 00:27:24 -0300

    Documented (and tested) the two implementations of Int: one optimized for sizes of 1, 2, 4, and 8 bytes, the other, slower but more flexible that can handle arbitrary sizes.

--- src/field.py
+++ src/field.py
@@ -26,62 +26,64 @@ class Int(Field):
       self.byte_count = byte_count
       self.is_signed = signed
       self.is_bigendian = (endianess in ('big', 'network')) or (endianess == 'local' and sys.byteorder == 'big')
-      self.base = 2**(byte_count*8) - 1 #base minus 1
+
+   def compile(self, field_name):
+      Field.compile(self, field_name)
+      
+      if self.byte_count in (1, 2, 4, 8): 
+         code = {1:'B', 2:'H', 4:'I', 8:'Q'}[self.byte_count]
+         if self.is_signed:
+            code = code.lower()
+
+         fmt = (">" if self.is_bigendian else "<") + code
+         s = struct.Struct(fmt)
+         del self.is_bigendian
+         del self.is_signed
+
+         def _unpack(raw_data):
+            return s.unpack(raw_data)[0]
+
+         def _pack(integer):
+            return s.pack(integer)
+
+      else:
+         base = 2**(self.byte_count*8) 
+         xcode = ("%0" + str(self.byte_count*2) + "x")
+         def _unpack(raw_data):
+            if not self.is_bigendian:
+               raw_data = raw_data[::-1]
+            
+            num = int(raw_data.encode('hex'), 16)
+            if self.is_signed and ord(raw_data[0]) > 127:
+               num = -(base - num)
+
+            return num
+
+         def _pack(integer):
+            num = (base + integer + 1) if integer < 0 else integer
+
+            data = (xcode % num).decode('hex')
+            if not self.is_bigendian:
+               data = data[::-1]
+
+            return data
+
+      self._unpack = _unpack
+      self._pack = _pack
 
    def init(self, packet, defaults):
       self.setval(packet, defaults.get(self.field_name, self.default))
 
    def from_raw(self, packet, raw, offset=0):
       raw_data = raw[offset:offset+self.byte_count]
-
-      if self.byte_count in (1, 2, 4, 8): 
-         code = {1:'B', 2:'H', 4:'I', 8:'Q'}[self.byte_count]
-         if self.is_signed:
-            code = code.lower()
-
-         fmt = (">" if self.is_bigendian else "<") + code
-         s = struct.Struct(fmt)
-
-         integer = s.unpack(raw_data)[0]
-
-      else:
-         if not self.is_bigendian:
-            raw_data = raw_data[::-1]
-         
-         num = int(raw_data.encode('hex'), 16)
-         if self.is_signed and ord(raw_data[0]) > 127:
-            num = -(self.base - num)
-
-         integer = num
-
+      integer = self._unpack(raw_data)
       self.setval(packet, integer)
 
       return self.byte_count
 
    def to_raw(self, packet):
       integer = self.getval(packet)
-      if self.byte_count in (1, 2, 4, 8): 
-         code = {1:'B', 2:'H', 4:'I', 8:'Q'}[self.byte_count]
-         if self.is_signed:
-            code = code.lower()
-
-         fmt = (">" if self.is_bigendian else "<") + code
-         s = struct.Struct(fmt)
-
-         raw = s.pack(integer)
-
-      else:
-         val = integer
-         if val < 0:
-            num = self.base + val + 1
-         else:
-            num = val
-
-         data = (("%0" + str(self.byte_count*2) + "x") % num).decode('hex')
-         if not self.is_bigendian:
-            data = data[::-1]
-
-         raw = data
+      raw = self._pack(integer)
 
       return raw
 
