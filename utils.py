030a6daa9dc38f011f8e2113bd1b768ce0d8d445
Martin Di Paola <martinp.dipaola@gmail.com>
2016-08-11 00:27:27 -0300

    Improved the inspect helper to be recursive.

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import array
+from bisturi.packet import Packet 
 
-def inspect(packet):
-   print packet.__class__.__name__
-   print "=" * len(packet.__class__.__name__)
+def inspect(packet, indent=""):
+   print "%s%s" % (indent, packet.__class__.__name__)
+   print "%s%s" % (indent, "=" * len(packet.__class__.__name__))
    for name, _ in packet.get_fields():
       value = getattr(packet, name)
       if isinstance(value, array.array):
@@ -11,5 +12,22 @@ def inspect(packet):
       if isinstance(value, basestring):
          value = ':'.join(x.encode('hex') for x in value)
 
-      print "%s = %s" % (name, value)
+      if isinstance(value, Packet):
+         print "%s%s:" % (indent, name)
+         inspect(value, indent+"  ")
+         value = None
+
+      if isinstance(value, (list, tuple)):
+         if value:
+            print "%s%s: [" % (indent, name)
+            for subvalue in value:
+               inspect(subvalue, indent+"  ")
+            print "%s]" % (indent, )
+         else:
+            print "%s%s: []" % (indent, name)
+
+         value = None
+
+      if value is not None:
+         print "%s%s = %s" % (indent, name, value)
 
