068864ab5e4c806f61cf8043127ac8041b4e2638
Martin Di Paola <martinp.dipaola@gmail.com>
2017-02-06 09:58:12 -0300

    Re-idented the example.

--- examples/dns.py
+++ examples/dns.py
@@ -8,40 +8,40 @@ import copy
 
 # RFC 1034, 1035
 class Label(Packet):
-   length = Int(1)
-   name   = Ref(lambda pkt, **k: Int(1) if pkt.is_compressed() else Data(pkt.length),
+    length = Int(1)
+    name   = Ref(lambda pkt, **k: Int(1) if pkt.is_compressed() else Data(pkt.length),
                     default='')
 
-   def is_root(self):
-      return self.length == 0
+    def is_root(self):
+        return self.length == 0
 
-   def is_compressed(self):
-      return self.length & 0xc0 == 0xc0
+    def is_compressed(self):
+        return self.length & 0xc0 == 0xc0
 
-   def offset(self):
-      if not self.is_compressed():
-         raise Exception()
+    def offset(self):
+        if not self.is_compressed():
+            raise Exception()
 
-      return (self.length & (~0xc0) << 8) + self.name
+        return (self.length & (~0xc0) << 8) + self.name
 
-   def uncompressed_name(self, raw, offset=0):
-      if not self.is_compressed():
-         return self.name
-      else:
-         builder = _Builder.unpack(raw, offset=self.offset())
-         return ".".join(n.uncompressed_name(raw, offset) for n in builder.name)
+    def uncompressed_name(self, raw, offset=0):
+        if not self.is_compressed():
+            return self.name
+        else:
+            builder = _Builder.unpack(raw, offset=self.offset())
+            return ".".join(n.uncompressed_name(raw, offset) for n in builder.name)
             
 class _Builder(Packet):
     name = Ref(Label).repeated(until=lambda pkt, **k: pkt.name[-1].is_root() or pkt.name[-1].is_compressed())
          
 
 class ResourceRecord(Packet):
-   name   = Ref(Label).repeated(until=lambda pkt, **k: pkt.name[-1].is_root() or pkt.name[-1].is_compressed())
-   type_  = Int(2, default=1)
-   class_ = Int(2, default=1)
-   ttl    = Int(4)
-   length = Int(2)
-   data   = Data(length)
+    name   = Ref(Label).repeated(until=lambda pkt, **k: pkt.name[-1].is_root() or pkt.name[-1].is_compressed())
+    type_  = Int(2, default=1)
+    class_ = Int(2, default=1)
+    ttl    = Int(4)
+    length = Int(2)
+    data   = Data(length)
 
 '''
 (from https://www.ietf.org/rfc/rfc1035.txt)
@@ -71,9 +71,9 @@ RDATA           a variable length string of octets that describes the
 '''
 
 class Question(Packet):
-   name   = Ref(Label).repeated(until=lambda pkt, **k: pkt.name[-1].is_root() or pkt.name[-1].is_compressed())
-   type_  = Int(2, default=1)
-   class_ = Int(2, default=1)
+    name   = Ref(Label).repeated(until=lambda pkt, **k: pkt.name[-1].is_root() or pkt.name[-1].is_compressed())
+    type_  = Int(2, default=1)
+    class_ = Int(2, default=1)
 
 '''
 QNAME           a domain name represented as a sequence of labels, where
@@ -93,28 +93,28 @@ QCLASS          a two octet code that specifies the class of the query.
 '''
 
 class Message(Packet):
-   id       = Int(2)    # query's id, it's copied in the corresponding reply to match up
+    id       = Int(2)    # query's id, it's copied in the corresponding reply to match up
 
-   is_query = Bits(1)   # a message can be a query or a reply
-   opcode   = Bits(4)
+    is_query = Bits(1)   # a message can be a query or a reply
+    opcode   = Bits(4)
 
-   is_authoritative  = Bits(1) # is an authoritative answer?
-   was_truncated     = Bits(1) # was the message truncated due its excessive length?
-   request_recursion = Bits(1) # the query desires to be resolved recursively; it's copied in the reply
+    is_authoritative  = Bits(1) # is an authoritative answer?
+    was_truncated     = Bits(1) # was the message truncated due its excessive length?
+    request_recursion = Bits(1) # the query desires to be resolved recursively; it's copied in the reply
    
-   is_recursion_available = Bits(1) # is the server supporting recursion? it's set/cleared in the reply
-   reserved      = Bits(3)
-   response_code = Bits(4)
+    is_recursion_available = Bits(1) # is the server supporting recursion? it's set/cleared in the reply
+    reserved      = Bits(3)
+    response_code = Bits(4)
 
-   qd_count = Int(2)
-   an_count = Int(2)
-   ns_count = Int(2)
-   ar_count = Int(2)
+    qd_count = Int(2)
+    an_count = Int(2)
+    ns_count = Int(2)
+    ar_count = Int(2)
 
-   questions   = Ref(Question).repeated(qd_count)
-   answers     = Ref(ResourceRecord).repeated(an_count)
-   authorities = Ref(ResourceRecord).repeated(ns_count)
-   additionals = Ref(ResourceRecord).repeated(ar_count)
+    questions   = Ref(Question).repeated(qd_count)
+    answers     = Ref(ResourceRecord).repeated(an_count)
+    authorities = Ref(ResourceRecord).repeated(ns_count)
+    additionals = Ref(ResourceRecord).repeated(ar_count)
 
 DNS = Message #alias
 '''
@@ -209,62 +209,62 @@ NSCOUNT         an unsigned 16 bit integer specifying the number of name
 ARCOUNT         an unsigned 16 bit integer specifying the number of
                 resource records in the additional records section.
 '''
-# TODO agregar una forma de debuggear el parseo, field a field.
+
 if __name__ == '__main__':
-   from base64 import b16decode
+    from base64 import b16decode
 
-   raw_query = b16decode('fabc010000010000000000010377777706676f6f676c6503636f6d00000100010000291000000000000000', True)
-   raw_response = b16decode('fabc818000010006000400050377777706676f6f676c6503636f6d0000010001c00c000100010000006400044a7d8368c00c000100010000006400044a7d8369c00c000100010000006400044a7d836ac00c000100010000006400044a7d8393c00c000100010000006400044a7d8363c00c000100010000006400044a7d8367c010000200010000016f0006036e7331c010c010000200010000016f0006036e7332c010c010000200010000016f0006036e7334c010c010000200010000016f0006036e7333c010c08c000100010001397d0004d8ef200ac09e000100010000b3600004d8ef220ac0c20001000100010a7a0004d8ef240ac0b0000100010000db710004d8ef260a0000291000000000000000', True)
+    raw_query = b16decode('fabc010000010000000000010377777706676f6f676c6503636f6d00000100010000291000000000000000', True)
+    raw_response = b16decode('fabc818000010006000400050377777706676f6f676c6503636f6d0000010001c00c000100010000006400044a7d8368c00c000100010000006400044a7d8369c00c000100010000006400044a7d836ac00c000100010000006400044a7d8393c00c000100010000006400044a7d8363c00c000100010000006400044a7d8367c010000200010000016f0006036e7331c010c010000200010000016f0006036e7332c010c010000200010000016f0006036e7334c010c010000200010000016f0006036e7333c010c08c000100010001397d0004d8ef200ac09e000100010000b3600004d8ef220ac0c20001000100010a7a0004d8ef240ac0b0000100010000db710004d8ef260a0000291000000000000000', True)
 
-   query = Message.unpack(raw_query)
-   response = Message.unpack(raw_response)
+    query = Message.unpack(raw_query)
+    response = Message.unpack(raw_response)
 
-   assert query.id == response.id == 0xfabc
+    assert query.id == response.id == 0xfabc
    
-   assert query.opcode == query.is_authoritative == query.was_truncated == query.reserved == query.response_code == 0
-   assert response.opcode == response.is_authoritative == response.was_truncated == response.reserved == response.response_code == 0
+    assert query.opcode == query.is_authoritative == query.was_truncated == query.reserved == query.response_code == 0
+    assert response.opcode == response.is_authoritative == response.was_truncated == response.reserved == response.response_code == 0
 
-   assert query.is_query == 0
-   assert response.is_query == 1
+    assert query.is_query == 0
+    assert response.is_query == 1
 
-   assert query.request_recursion == 1 and query.is_recursion_available == 0
-   assert response.request_recursion == 1 and response.is_recursion_available == 1
+    assert query.request_recursion == 1 and query.is_recursion_available == 0
+    assert response.request_recursion == 1 and response.is_recursion_available == 1
    
-   assert query.qd_count == len(query.questions) == 1
-   assert query.an_count == len(query.answers) == 0
-   assert query.ns_count == len(query.authorities) == 0
-   assert query.ar_count == len(query.additionals) == 1
+    assert query.qd_count == len(query.questions) == 1
+    assert query.an_count == len(query.answers) == 0
+    assert query.ns_count == len(query.authorities) == 0
+    assert query.ar_count == len(query.additionals) == 1
  
-   assert response.qd_count == len(response.questions) == 1
-   assert response.an_count == len(response.answers) == 6
-   assert response.ns_count == len(response.authorities) == 4
-   assert response.ar_count == len(response.additionals) == 5
+    assert response.qd_count == len(response.questions) == 1
+    assert response.an_count == len(response.answers) == 6
+    assert response.ns_count == len(response.authorities) == 4
+    assert response.ar_count == len(response.additionals) == 5
    
 
-   the_question = query.questions[0]
-   assert list(map(lambda n: n.name, the_question.name)) == ['www', 'google', 'com', '']
+    the_question = query.questions[0]
+    assert list(map(lambda n: n.name, the_question.name)) == ['www', 'google', 'com', '']
 
-   the_question = response.questions[0]
-   assert list(map(lambda n: n.name, the_question.name)) == ['www', 'google', 'com', '']
+    the_question = response.questions[0]
+    assert list(map(lambda n: n.name, the_question.name)) == ['www', 'google', 'com', '']
  
-   BASE = "google.com."
-   W    = "www." + BASE
-   NS1  = "ns1." + BASE
-   NS2  = "ns2." + BASE
-   NS3  = "ns3." + BASE
-   NS4  = "ns4." + BASE
-   ROOT = ""
-   for resource_records, expecteds in zip(
+    BASE = "google.com."
+    W    = "www." + BASE
+    NS1  = "ns1." + BASE
+    NS2  = "ns2." + BASE
+    NS3  = "ns3." + BASE
+    NS4  = "ns4." + BASE
+    ROOT = ""
+    for resource_records, expecteds in zip(
             (response.questions, response.answers, response.authorities, response.additionals),
             ([W], [W]*6, [BASE]*4, [NS1, NS2, NS3, NS4, ROOT])):
       
-      assert len(resource_records) == len(expecteds)
-      for one_record, expected_name in zip(resource_records, expecteds):
-         labels = one_record.name 
-         name = ".".join([label.uncompressed_name(raw_response) for label in labels])
+        assert len(resource_records) == len(expecteds)
+        for one_record, expected_name in zip(resource_records, expecteds):
+            labels = one_record.name 
+            name = ".".join([label.uncompressed_name(raw_response) for label in labels])
          
-         assert name == expected_name
+            assert name == expected_name
    
-   assert query.pack() == raw_query
-   assert response.pack() == raw_response
+    assert query.pack() == raw_query
+    assert response.pack() == raw_response
 
