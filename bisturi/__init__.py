3f3ac58baa45e76694c182a99a73c3bd3187efaf
Martin Di Paola <martinp.dipaola@gmail.com>
2018-02-20 01:38:18 +0000

    Bump version.

---
+++ bisturi/__init__.py
@@ -0,0 +1,7 @@
+'''Parse binary data painless describing the packets' structures in python classes: no need to write 'for' loops or nested 'if' conditionals.'''
+
+__version__ = "0.5.0"
+
+_author  = 'Gehn'
+_license = 'GNU LGPLv3'
+_url = 'https://github.com/gehnch/bisturi'
