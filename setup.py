eddd084cf8c0cbf67433ca0b9a60b643ef4ed484
Martin Di Paola <martinp.dipaola@gmail.com>
2021-08-03 13:44:00 +0000

    New setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,50 @@
 # https://packaging.python.org/en/latest/distributing.html
 # https://github.com/pypa/sampleproject
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from codecs import open
 from os import path, system
 
-import sys
+import sys, re
 
 here = path.abspath(path.dirname(__file__))
 
-system('''pandoc -o '%(dest_rst)s' '%(src_md)s' ''' % {
-            'dest_rst': path.join(here, 'README.rst'),
-            'src_md':   path.join(here, 'README.md'),
-            })
+try:
+    system('''pandoc -f markdown-raw_html -o '%(dest_rst)s' '%(src_md)s' ''' % {
+                'dest_rst': path.join(here, 'README.rst'),
+                'src_md':   path.join(here, 'README.md'),
+                })
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+
+    # strip out any HTML comment|tag
+    long_description = re.sub(r'<!--.*?-->', '', long_description,
+                                                flags=re.DOTALL|re.MULTILINE)
+    long_description = re.sub(r'<img.*?src=.*?>', '', long_description,
+                                                flags=re.DOTALL|re.MULTILINE)
+
+    with open(path.join(here, 'README.rst'), 'w', encoding='utf-8') as f:
+        f.write(long_description)
+
+except:
+    print("Generation of the documentation failed. " + \
+          "Do you have 'pandoc' installed?")
+
+    long_description = __doc__
 
 # load __version__, __doc__, _author, _license and _url
 exec(open(path.join(here, 'bisturi', '__init__.py')).read())
 
-system("find . -name '_[^_]*' -delete")
+long_description = __doc__
+
+install_deps=[
+        'xview',
+        ]
+
+optional_deps=[
+        ]
+
 setup(
     name='bisturi',
     version=__version__,
@@ -28,7 +52,7 @@ setup(
     description=__doc__,
     long_description=long_description,
 
-    url=_url,
+    #url=_url,
 
     # Author details
     author=_author,
@@ -38,22 +62,28 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Compilers',
+        'Topic :: Software Development :: Disassemblers',
+        'Topic :: System :: Networking',
 
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
 
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-
-        'Topic :: Software Development :: Code Generators',
-        'Topic :: Software Development :: Compilers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 
-    python_requires='>=2.6',
+    python_requires='>=3.6',
+    install_requires=install_deps + optional_deps,
 
-    keywords='parse parsing binary network file compiler code generator',
+    keywords='parsing parser dissector binary packet structure struct',
 
     packages=['bisturi'],
 )
